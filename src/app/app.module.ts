36b72b3e60c6899778f4472205ea8b69f4b48026
Bob Pham <bob.pham@hexagonsi.com>
2019-11-27 10:43:45 +1100

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // for anything that we use in the template
   declarations: [
     AppComponent
   ],
