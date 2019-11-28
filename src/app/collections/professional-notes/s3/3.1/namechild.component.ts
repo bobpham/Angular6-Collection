2b2e5780fc77ace8efdd563ad4f4c7b4a25b256b
Bob Pham <bob.pham@hexagonsi.com>
2019-11-28 17:11:36 +1100

    work in progress

--- src/app/collections/professional-notes/s3/3.1/namechild.component.ts
+++ src/app/collections/professional-notes/s3/3.1/namechild.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 @Component({
     selector: 'name-child',
-    template: '<h3>"{{name}}"</h3>'
+    template: '<h3>"{{childName}}"</h3>'
 })
 export class NameChildComponent {
     private _name = '';
     @Input()
-    set name(name: string) {
+    set childName(name: string) {
         this._name = (name && name.trim()) || '<no name set>';
     }
-    get name(): string { return this._name; }
+    get childName(): string { return this._name; }
 }
