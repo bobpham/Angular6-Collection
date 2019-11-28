a6a19548fd408f50aa5ca24217a11458e64b13fa
Bob Pham <bob.pham@hexagonsi.com>
2019-11-28 15:27:39 +1100

    work in progress

--- src/app/collections/professional-notes/wrapper.module.ts
+++ src/app/collections/professional-notes/wrapper.module.ts
@@ -1,18 +1,24 @@
 
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { WrapperComponent } from './wrapper.component';
 import { HeroComponent} from './s2/2.3/hero.component';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { NameParentComponent} from './s3/3.1/nameparent.component';
+import { NameChildComponent} from './s3/3.1/namechild.component';
 
 @NgModule({
     // for anything that we use in the template
     declarations: [
         WrapperComponent,
-        HeroComponent
+        HeroComponent,
+        NameChildComponent,
+        NameParentComponent
     ],
     imports: [
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CommonModule
     ],
     exports:[WrapperComponent]
 
